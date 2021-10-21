938dda6983abd8bbff2e4de4e02f98a1fa90a7f5
tuanhoanganh1 <92909012+tuanhoanganh1@users.noreply.github.com>
2021-10-21 13:30:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Kid';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc bạn Hòa 20/10 tràn ngập niềm vui và những nụ cười,ngày càng xinh đẹp hơn và thành công hơn. Mong điều đẹp nhất sẽ đến với Hòa trong hôm nay và cả những ngày sau.Hihi.Ngại quá!Xin lỗi vì chúc muộn :(';
-
+.
 
 const giftImage = 'hot-girl.png.jpg';
 const base64 = '';

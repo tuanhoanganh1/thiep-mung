cba434a4d27314ab1858cee7d349a50b63c8485a
tuanhoanganh1 <92909012+tuanhoanganh1@users.noreply.github.com>
2021-10-21 13:11:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc bạn Hòa 20/10 tràn ngập niềm vui và những 
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'hot-girl.png.jpd';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
